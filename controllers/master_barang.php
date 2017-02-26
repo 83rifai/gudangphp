cf78ec67aedc15b229a04f740cbcf70d0e28535c
83rifai <83rifai@gmail.com>
2017-02-26 19:21:51 +0700

    update controller

--- controllers/master_barang.php
+++ controllers/master_barang.php
@@ -19,7 +19,7 @@ if($act == "add"){
 }else if($act == "del"){
 	if($_GET['id']){
 		$query =  mysql_query("DELETE FROM master_produk where id = ".$_GET['id']." ");
-		if($queury){
+		if($query){
 			echo "Berhasil Dihapus";
 		}else{
 			echo "Gagal Dihapus";
@@ -28,9 +28,18 @@ if($act == "add"){
 		echo "Gagal Dihapus Data Tidak ditemukan ";
 	}
 }else if($act == "edit"){
-	
-}else if($act == "getEdit"){
-	
+	if($_POST){
+		$query = mysql_query("UPDATE master_produk (nama,jenis_id,konversi_satuan_id,warna) 
+			SET nama = ('".$_POST['nama']."', jenis_id = '".$_POST['jenis_id']."', konversi_satuan_id = '".$_POST['konversi_satuan_id']."',warna = '".$_POST['warna']."')
+			WHERE id = ".$_POST['id']."
+			");
+
+			if($query){
+				echo "Berhasil Disimpan";
+			}else{
+				echo "Gagal Disimpan";
+		}
+	}
 }
 
 
