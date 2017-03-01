869d90d8a706fb774d6a345394701b2bed612eae
83rifai <83rifai@gmail.com>
2017-03-01 07:29:36 +0700

    update

--- mod/transaksi/brg_masuk.php
+++ mod/transaksi/brg_masuk.php
@@ -86,6 +86,7 @@ function addbarang1(str,vlue) {
 		 }
   
 var no	= 1;
+
 function addbarang(str,vlue) {
 			var id	   = document.getElementById("kd_tr_masuk").value;
 			var nama   = $("#nm_barang").val();
@@ -282,134 +283,151 @@ $(document).ready(function(){
  
 <?php
 }
-    /////// function tambah ////////
+    // this function for add new data trans
     function doAdd(){
-    $aksi = $_GET[act]; 
-?>
-	<div class="tabelku col-md-11">
-	<div class="box-body">
-    
-		<form action="mod\transaksi\aksi\tr_masuk.php" method="post" id="brg_masuk1" enctype="multipart/form-data" >
-		 <?php
-				$thn_skr = date('d-m-Y');
-				$date	= explode('-',$thn_skr);
-				$tgl	= $date[0];
-				$bln	= $date[1];
-				$thn	= $date[2];
-				$th		= substr($thn , 2 ,2);
+    $aksi = $_GET['act']; 
+
+	$thn_skr = date('d-m-Y');
+	$date	= explode('-',$thn_skr);
+	$tgl	= $date[0];
+	$bln	= $date[1];
+	$thn	= $date[2];
+	$th		= substr($thn , 2 ,2);
 				
-				$no_max=mysql_query("SELECT ifnull(max(no),0)+1 AS reg  FROM tr_brg_masuk");
-	            $n=mysql_fetch_array($no_max);   
+	$no_max=mysql_query("SELECT ifnull(max(id),0)+1 AS reg  FROM trans_produk_masuk_header");
+	$n=mysql_fetch_array($no_max);   
                 
-				$no_tr= "BM-"."$tgl$bln$th"."00".$n[reg];
-				
-            ?>       
-        <input type="hidden" name="id" id="id" value="<?php echo"$_GET[id]";?>" />
-        <input type="hidden" name="aksi" id="aksi" value="<?php echo"$aksi";?>" />
-          <table>	
-			<div class="form-group">
-                <div class="col-md-12">
-					<label style="font-size:24px;">Input Barang Masuk</label> 
+	$no_tr= "BM-"."$tgl$bln$th"."00".$n[reg];
+
+?>
+<form method="post" action="#" id="form-transaksi">
+	<div class="col-md-11">
+		<fieldset style="border: 1px solid #c0c0c0; padding: 15px;">
+				<legend style="background-color: #c0c0c0; font-size: 11pt; border: none; margin: 5px; padding: 5px; width: auto; ">Transaksi Barang Masuk</legend>
+		<div class="col-md-4">
+			<label>Nomor Transaksi</label>
+			<input class="form-control" placeholder="Nomor Transaksi" name="kd_tr_masuk" id="kd_tr_masuk" type="text" value="<?php echo"$no_tr"?>" readonly="">
+		</div>
+		<div class="col-md-4">
+			<label>Nama Supplier</label>
+			<select name="suplier_id" class="form-control">
+				<option value="">-Pilih Supplier-</option>
+					<?php
+						$query = mysql_query("select * from master_suplier");
+						while($result = mysql_fetch_assoc($query) ){
+								
+						echo "<option value='".$result['id']."'>".$result['nama']."</option>";
+					}
+				?>
+			</select>
+		</div>
+		<div class="col-md-4">
+			<label>Tanggal</label>
+			<span>
+				<div class='input-group date' id='tgl' name='tgl' >
+	                <input type='text' class="form-control" id="tgl" value="<?=date('Y-m-d')?>" name="tgl" />
+	            <span class="input-group-addon">
+	            <span class="glyphicon glyphicon-calendar"></span>
+	            </span>
+	            </div>
+			</span>
+		</div>
+		<!-- this header form end -->
+		<hr />
+
+			<fieldset style="border: 1px solid #c0c0c0; padding: 15px; margin-top: 20px;">
+				<legend style="background-color: #c0c0c0; font-size: 11pt; border: none; margin: 5px; padding: 5px; width: auto; ">Detail Barang</legend>
+				<div class="table-responsive">
+					<table class="table table-bordered">
+						<thead>
+							<tr>
+								<th>No</th>
+								<th>Nama Barang</th>
+								<th>Jumlah</th>
+								<th width="60px"></th>
+							</tr>
+							<tr>
+								<td width="60px">#</td>
+								<td>	
+								<select name="barang" class="form-control">
+										<option value="">-Pilih Barang-</option>
+											<?php
+												$queries = mysql_query("select * from master_produk");
+												while($results = mysql_fetch_assoc($queries) ){		
+												echo "<option value='".$results['id']."'>".$results['nama']."</option>";
+											}
+										?>
+									</select>
+								</td>
+								<td width="150px">
+									<input type="text" name="jumlah" class="form-control" placeholder="Jumlah Produk">
+								</td>
+								<td><center><button type="button" id="BTN-TAMBAH" class="btn btn-primary btn-sm">Tambah</button></center></td>
+							</tr>
+						</thead>
+						<tbody id="Details">
+							
+						</tbody>
+					</table>
 				</div>
-			</div>	
-			<br><br><br>
-			<div class="form-group">
-				<div class="col-md-1">
-					<label>Tanggal</label> 
-				</div>
-				<div class="col-md-4">		
-					<div class='input-group date' id='tgl' name='tgl' >
-                        <input type='text' class="form-control" id="tgl" name="tgl" />
-                        <span class="input-group-addon">
-                            <span class="glyphicon glyphicon-calendar"></span>
-                        </span>
-                    </div>
-				</div>
-				<div class="col-md-2"></div>
-				<div class="col-md-2">
-					<label>No Transaksi</label> 
-				</div>
-				<div class="col-md-3">
-					<input class="form-control" placeholder="Nomor Transaksi" name="kd_tr_masuk" id="kd_tr_masuk" type="text" value="<?php echo"$no_tr"?>" readonly="">
-				</div>	
-			</div>
-            <br><br>
-			<div class="form-group col-lg-12">
-				<div class="col-md-1">
-					<label>Suplier</label> 
-				</div>
-				<div class="col-md-3">
-					<?php echo"<select name='id_suplier' id='id_suplier' class='form-control'>
-                                  <option value='' selected>- Pilih Suplier -</option>";
-                   	   $tampil=mysql_query("SELECT * FROM suplier ");
-                   	   while($v=mysql_fetch_array($tampil)){
-                            echo "<option value='$v[id]'>$v[nm_suplier]</option>";
-                       }
-                            echo "</select>";
-					?>
-				</div>
-			</div>
-			<br><br>
-			<div class="form-group">
-				<div class="col-md-1">
-					<label>Barang</label> 
-				</div>
-				<div class="col-md-3">
-					<input class="form-control" placeholder="Nama Barang" name="nm_barang[]" id="nm_barang" type="text" readonly="">
-					<input class="form-control" name="kd_barang[]" id="kd_barang" type="hidden" >
-					<input class="form-control" name="id_satuan[]" id="id_satuan" type="hidden" >
-				</div>
-				<div class="col-md-1">
-					<input class="btn btn-info" type="button" value="cari" id="cari" onclick="tombol2()"> 
-				</div>
-			</div>
-			<br><p></br>
-			<div class="form-group">
-				
-				<div class="col-md-1">
-					<label>Satuan</label> 
-				</div> 
-				<div class="col-md-3">
-					<input class="form-control" placeholder="Satuan Barang" name="kd_satuan[]" id="kd_satuan" type="text" readonly="">
-				   <input class="form-control" name="satuan[]" id="satuan" type="hidden" >
-				   <!--	<select class="form-control2" placeholder="Satuan Barang" name="kd_satuan[]" id="kd_satuan" type="text" value=""></select> -->
-				</div>
-				<div class="col-md-1">
-					<input class="btn btn-info" type="button" value="cari" id="cari" onclick="tombol3()"> 
-				</div>
-			</div>
-			<br><p></br>
-			<div class="form-group">
-				<div class="col-md-1">
-					<label>Jumlah</label> 
-				</div>
-				<div class="col-md-3">
-					<input class="form-control" placeholder="Jumlah Barang" name="jumlah[]" id="jumlah" type="text" >
-				</div>
-			</div>
-			<br></br>
-		   </table>	
-            <div class="form-group" >
-               <input type="submit" class="btn btn-info" value="simpan">
-			   <input type="button" class="btn btn-info" value="tambah" onclick="addbarang()">
-               <input type="button" class="btn btn-danger" onClick='self.history.back()'  value="Keluar">
-			</div>  
-		   	<table class="table table-bordered">
-				<thead>
-				  <tr id="dt_barang">
-					<th width="75%">Nama Barang</th>
-					<th width="10%">Jumlah</th>
-					<th width="10%">Satuan</th>
-					<th width="5%">Hapus</th>
-				  </tr>
-				</thead>
-				<tbody>
-					<tr id="dat"></tr>
-				</tbody>
-			  </table>	
-		</form>
+			</fieldset>
+		</fieldset>
+
 	</div>
-</div>
+
+	<div class="col-md-11">
+		<fieldset style="border: 1px solid #c0c0c0; padding: 15px; margin-top: 20px;">
+			<input type="submit" name="submit" class="btn btn-success btn-md" value="Simpan">
+		</fieldset>
+	</div>
+
+</form>
+
+<script type="text/javascript">
+	$(document).ready(function(){
+		$('#BTN-TAMBAH').click(function(){
+			var details = "";
+			var long = parseInt($('#Details').find('tr').length);
+			var index = long+1;
+			var int = 0;
+			var indexTR = '';
+			$('#Details').find('tr').each(function(){
+				int++;
+				if(long==int) {
+					var IDtr = $(this).attr('class').split('_');
+					indexTR = parseInt(IDtr[1])+1;
+				}
+			})
+
+			if(indexTR==''){
+				indexTR = 1;
+			}
+
+			details = "<tr class='list_"+indexTR+"' >";
+				details += "<td>"+indexTR+"</td>";
+				details += "<td>"+$('select[name=barang] :selected').text()+"</td>";
+				details += "<input type='hidden' name='data["+indexTR+"][master_produk_id]' value='"+$('select[name=barang] :selected').val()+"' >";
+				details += "<td>"+$('input[name=jumlah]').val()+"</td>";
+				details += "<input type='hidden' name='data["+indexTR+"][jumlah]' value='"+$('input[name=jumlah]').val()+"' >";
+				details += "<td></td>";
+			details += "</tr>";
+
+			$('#Details').append(details);
+		}); // end dom add append tr
+
+		$('#form-transaksi').submit(function(){
+			$.post('controllers/trans_produk_masuk.php?act=add',$('#form-transaksi').serialize(),function(data){
+			alert(data);
+			window.location.href = "media.php?mod=brg_masuk";
+		});
+
+			return false;
+		}); // end
+	}); // end
+
+
+</script>
+
 <?php
 }
 
