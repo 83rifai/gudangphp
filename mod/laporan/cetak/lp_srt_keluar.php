7d5475bdfa367c612cf084565b31535f7c8db035
83rifai <83rifai@gmail.com>
2017-03-07 09:35:24 +0700

    commit

--- mod/laporan/cetak/lp_srt_keluar.php
+++ mod/laporan/cetak/lp_srt_keluar.php
@@ -24,7 +24,23 @@ include"../../../mpdf/mpdf.php";
 $query = mysql_query("SELECT * FROM data_produk_keluar where trans_produk_keluar_id = ".$_GET['id']."  ");
 $mpdf	= new mPDF('utf-8', 'A4-P'); // Membuat file mpdf baru			
 
-$html = $_GET['id']."<table cellspacing = '0' style='border: 1px solid #c0c0c0; width:100%;' >";
+$html = "<center><h3>SURAT JALAN</h3></center>"; 
+
+$html .= "<table width='100%'>";
+	$html .= "<tr>";
+	$html .= "<td width='50%'>Dari :</td>";
+	$html .= "<td width='50%'>Tujuan :/td>";
+	$html .= "<td width='50%'>PT. RAMA BERKAH ABADI<br/>Griya Taman Sari T5</td>";
+	$html .= "<td width='50%'></td>";
+	$html .= "</tr>";
+$html .= "</table>";
+$html .= "<br/>";
+
+// $mpdf->SetHTMLHeader($header);
+
+$html .= "<hr/>";
+
+$html .= "<table cellspacing = '0' style='border: 1px solid #c0c0c0; width:100%;' >";
 	$no = 1;
 	$html .="<tr style='background: #f0f0f0;'>";
 	$html .="<th width='5%' aling='center'>No</th>";
@@ -34,6 +50,7 @@ $html = $_GET['id']."<table cellspacing = '0' style='border: 1px solid #c0c0c0; 
 	$html .="<th aling='center'>Satuan Produk</th>";
 	$html .="<th aling='center'>Pelanggan</th>";
 	$html .="</tr>";
+	
 	while ($result = mysql_fetch_assoc($query)) {
 		$html .="<tr>";
 		$html .="<td>".$no++."</td>";
@@ -44,6 +61,7 @@ $html = $_GET['id']."<table cellspacing = '0' style='border: 1px solid #c0c0c0; 
 		$html .="<td>".$result['pelanggan']."</td>";
 		$html .="</tr>";
 	}
+
 $html .= "</table>";
 
 $mpdf->WriteHTML($html);
